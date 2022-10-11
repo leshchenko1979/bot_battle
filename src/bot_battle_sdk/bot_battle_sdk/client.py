925409919493c1eda9df23326958bebce2c22c17
Alexey Leshchenko <leshchenko@gmail.com>
2022-10-11 14:08:44 +0300

    Updated client

--- src/bot_battle_sdk/bot_battle_sdk/client.py
+++ src/bot_battle_sdk/bot_battle_sdk/client.py
@@ -1,127 +1,93 @@
 import asyncio
+from pathlib import Path
 
 import httpx
-import websockets
 
 from .protocol import (
-    MoveMessage,
-    ResultMessage,
-    StartingMessage,
-    StateMessage,
-    ClientData,
+    NewGameResponse, NewGameResponseType, GameStateResponse
 )
+
+from logging import info, debug
 
 MAX_SOCKETS = 10
 STARTING_PORT = 8100
 
 
-class Client:
+class BotClient:
     def __init__(
         self,
         token: str,
         cls: type,
-        dispatcher_url="http://localhost:8200",
-        starting_port=STARTING_PORT,
-        max_sockets=MAX_SOCKETS,
+        dispatcher_url,
+        max_games: int = 10
     ):
         self.bot_id = None
         self.bot_token = token
         self.bot_cls = cls
 
-        self.dispatcher_url = dispatcher_url
-        self.starting_port = starting_port
-        self.max_sockets = max_sockets
+        self.dispatcher_url = Path(dispatcher_url)
         self.players = {}  # game_id: player
 
-        self.sockets = [None] * (max_sockets + 1)
+        self.semaphore = asyncio.Semaphore(max_games)
+        self.runners = set()  # set of tasks running games
+
+        self.set_up_http_client()
 
     def __repr__(self):
-        return f"<Client(id={self.bot_id})>"
+        return f"<BotClient(id={self.bot_id})>"
+
+    def set_up_http_client(self):
+        headers = {"Authorization": f"Bearer {self.token}"}
+        self.http_client = httpx.AsyncClient(
+            base_url=self.dispatcher_url, header=headers
+        )
+
+    async def get(self, path, **kwargs):
+        return await self.http_client.get(path, **kwargs)
+
+    async def post(self, path, **kwargs):
+        return await self.http_client.post(path, **kwargs)
 
     async def run(self):
-        try:
-            tasks = [
-                self.set_up_socket(i, self.game_runner) for i in range(self.max_sockets)
-            ]
-            tasks += [self.set_up_socket(self.max_sockets, self.health_checker)]
-            await asyncio.wait(tasks)
-            print(f"{self.max_sockets + 1} sockets set up")
+        await asyncio.gather(self.request_games(), self.run_games())
 
-            await self.keep_connection()
-
-        except KeyboardInterrupt:
-            print("Interrupted by user")
-
-        for socket in self.sockets:
-            socket.close()
-
-    async def keep_connection(self):
+    async def request_games(self):
+        info("Requesting new games")
         while True:
-            print(f"Connecting to {self.dispatcher_url}")
+            response = NewGameResponse(await self.get("/new_game").json())
 
-            # register self with the dispatcher
-            async with httpx.AsyncClient() as client:
-                result = await client.post(
-                    self.dispatcher_url,
-                    data=ClientData(
-                        token=self.bot_token,
-                        starting_port=self.starting_port,
-                        max_sockets=self.max_sockets,
-                    ).json(),
-                )
+            if response.response_type == NewGameResponseType.WAIT:
+                info(f"Asked to wait for {response.wait_for} seconds")
+                await asyncio.sleep(response.wait_for)
+                continue
 
-            print("Connection result:", result)
+            await self.semaphore.acquire()
+            info(f"Starting new game {response.game_id}")
+            self.runners.add(asyncio.create_task(self.game_runner(response.game_id)))
 
-            if result.status_code == 200:
-                json = result.json()
-                self.bot_id = json["bot_id"]
+    async def run_games(self):
+        info("Running games")
+        while True:
+            if not(self.runners):
+                asyncio.sleep(1)
+                continue
 
-            await asyncio.sleep(10)
+            done, self.runners = asyncio.wait(self.runners, return_when=asyncio.FIRST_COMPLETED)
 
-    async def set_up_socket(self, i: int, handler):
-        port = self.starting_port + i
-        self.sockets[i] = await websockets.serve(handler, "localhost", port)
+            for _ in range(len(done)):
+                self.semaphore.release()
 
-    async def game_runner(self, websocket):
-        game_id = None
-        try:
-            # start
-            starting_msg = StartingMessage.parse_raw(await websocket.recv())
-            game_id, side = starting_msg.game_id, starting_msg.side
+    async def game_runner(self, game_id):
+        player = self.bot_cls()
 
-            self.players[game_id] = self.bot_cls(side)
-            print(f"Started a game {game_id}, {side}")
+        # request initial state
+        response = GameStateResponse(await self.get("/games/{game_id}/latest").json())
 
-            # make moves
-            while True:
-                msg = await websocket.recv()
-                try:
-                    state_msg = StateMessage.parse_raw(msg)
-                except Exception:
-                    break
+        while not response.result:
+            # make a move
+            move = player.make_move(response.state)
+            response = GameStateResponse(await self.post(f"/games/{game_id}/move?move={move}").json())
 
-                # print(f"Received state {state_msg}")
-
-                move = self.players[game_id].make_move(state_msg.state)
-                move_msg = MoveMessage(move=move).json()
-
-                # print(f"Sending move: {move}")
-                await websocket.send(move_msg)
-
-            # print results
-            result = ResultMessage.parse_raw(msg)
-            print(f"{game_id}: {result}")
-
-        except (
-            websockets.exceptions.WebSocketException,
-            asyncio.exceptions.IncompleteReadError,
-        ) as e:
-            print(f"Websocket error: {e}")
-
-        finally:
-            if game_id and game_id in self.players:
-                del self.players[game_id]
-
-    @staticmethod
-    async def health_checker(websocket):
-        pass
+        # show results
+        info(f"Game {game_id} finished")
+        info(f"{response.result}")
--- src/bot_battle_sdk/bot_battle_sdk/protocol.py
+++ src/bot_battle_sdk/bot_battle_sdk/protocol.py
@@ -7,9 +7,9 @@ from enum import Enum
 
 
 class GameResultType(Enum):
-    ABORTED = 0
-    VICTORY = 1
-    TIE = 2
+    ABORTED = "aborted"
+    VICTORY = "victory"
+    TIE = "tie"
 
 
 class GameResult(BaseModel):
@@ -19,11 +19,19 @@ class GameResult(BaseModel):
 
 class GameStateResponse(BaseModel):
     state: State
+    your_side: Side = None
     result: GameResult = None
 
 
-class WaitResponse(BaseModel):
-    seconds: int
+class NewGameResponseType(Enum):
+    NEW_GAME_CREATED = "new_game_created"
+    WAIT = "wait"
+
+
+class NewGameResponse(BaseModel):
+    response_type: NewGameResponseType
+    game_id: UUID4 = None
+    wait_for: int = None
 
 
 class GameListResponse(BaseModel):
