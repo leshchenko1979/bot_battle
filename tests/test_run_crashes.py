6db6d03136cffdb3df1e73a01bb31a26160c76c0
Alexey Leshchenko <leshchenko@gmail.com>
2022-10-22 13:25:47 +0300

    Add states to result in case of crashes

--- tests/test_run_crashes.py
+++ tests/test_run_crashes.py
@@ -1,9 +1,16 @@
+import time
+
 import pytest
 from botbattle import PlayerAbstract
-import time
-from runner.runner import get_game_results, RunnerException, HangsException, InvalidMoveException, RaisesException, MoveBrakesRulesException
-from sample_bots.random_player import RandomPlayer
 from botbattle.players import make_code
+from runner.runner import (
+    ERROR_MESSAGES,
+    HangsException,
+    InvalidMoveException,
+    MoveBrakesRulesException,
+    RaisesException,
+    get_game_results,
+)
 
 
 class Hangs(PlayerAbstract):
@@ -11,30 +18,42 @@ class Hangs(PlayerAbstract):
         for _ in range(10):
             time.sleep(0.1)
 
+
 class InvalidMove(PlayerAbstract):
     def make_move(self, board):
         return "oops"
+
 
 class Raises(PlayerAbstract):
     def make_move(self, board):
         raise RuntimeError
 
+
 class MoveBrakesRules(PlayerAbstract):
     def make_move(self, board):
         return 0
 
-@pytest.mark.parametrize("player, expected_exception", [
-    [Hangs, HangsException],
-    [InvalidMove, InvalidMoveException],
-    [Raises, RaisesException],
-    [MoveBrakesRules, MoveBrakesRulesException]
-])
-async def test_run_crashes(player, expected_exception):
-    try:
-        await get_game_results(make_code(player), make_code(player))
+
+@pytest.mark.parametrize(
+    "player, expected_exception, tb_info, move_info",
+    [
+        [Hangs, HangsException, False, False],
+        [InvalidMove, InvalidMoveException, True, True],
+        [Raises, RaisesException, True, False],
+        [MoveBrakesRules, MoveBrakesRulesException, True, True],
+    ],
+)
+async def test_run_crashes(player, expected_exception, tb_info, move_info):
+    log_dict = await get_game_results(make_code(player), make_code(player))
+    if "exception" not in log_dict:
         raise RuntimeError("Should have thrown an exception")
 
-    except RunnerException as exc:
-        # print(exc.args)
-        # assert False
-        assert isinstance(exc, expected_exception)
+    exc = log_dict["exception"]
+
+    assert ERROR_MESSAGES[expected_exception] in exc.msg
+
+    if tb_info:
+        assert "\nTraceback" in exc.msg
+
+    if not move_info:
+        assert exc.move is None
